5981ad1715f9975eb2fe70b7f69b2cacbe02e172
GOTERmcDev <davidchaplygin8@gmail.com>
2025-08-21 16:48:13 +0300

    Update-210825

--- RetroRace96/font.cpp
+++ RetroRace96/font.cpp
@@ -3,7 +3,8 @@
 static sf::Font main_font;
 
 void load_fonts() {
-    main_font.openFromFile("Assets/MainFont.OTF"); // Main font register
+    if (!main_font.openFromFile("Assets/MainFont.OTF")) // Main font register
+        throw std::runtime_error("error while loading font file");
 }
 
 void draw_text(sf::RenderWindow& wnd, const std::string& txt, int x, int y, int size, sf::Color color) {
